4d0f77c804d53af94b9aaf1a5ce35901c3ce0927
atsushi <london.rightside@gmail.com>
2019-10-05 12:42:57 +0900

    コンテキストメニュークリックでその行に紐づいている社員オブジェクトの社員名をダイアログ表示する。

--- TryContextMenu/ViewModels/MainPageViewModel.cs
+++ TryContextMenu/ViewModels/MainPageViewModel.cs
@@ -36,5 +36,7 @@ namespace TryContextMenu.ViewModels
             };
 
         }
+
+
     }
 }
--- TryContextMenu/Views/MainPage.xaml.cs
+++ TryContextMenu/Views/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TryContextMenu.Models;
 using Xamarin.Forms;
 
 namespace TryContextMenu
@@ -16,6 +18,26 @@ namespace TryContextMenu
         public MainPage()
         {
           InitializeComponent();
+        }
+
+        /// <summary>
+        /// 削除コンテキストメニュークリックのイベントハンドラ。
+        /// </summary>
+        /// <param name="sender">クリックされたMenuItem。</param>
+        /// <param name="e">なし。</param>
+        void OnDeleteMenuClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("OnDeleteMenuClicked");
+
+            // クリックされたMenuItemに紐づいている社員オブジェクトを取得する。
+            MenuItem target = sender as MenuItem;
+            if (target == null) return;
+            Employee emp = target.BindingContext as Employee;
+            if (emp == null) return;
+
+            // ダイアログでコンテキストメニューを表示した行の社員名を表示する。
+            DisplayAlert(target.Text,
+                string.Format($"{emp.FullName}の削除コンテキストメニューがクリックされました。"), "ok");
         }
     }
 }
