6a6da045f0feb5b0aac5217674b53ccf06632522
atsushi <london.rightside@gmail.com>
2019-10-05 11:48:49 +0900

    ListViewに社員リストを表示する。

---
+++ TryContextMenu/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TryContextMenu.Models
+{
+    /// <summary>
+    /// 社員クラス。
+    /// </summary>
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+
+        /// <summary>
+        /// 社員名。
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
--- TryContextMenu/ViewModels/MainPageViewModel.cs
+++ TryContextMenu/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using TryContextMenu.Models;
+
 namespace TryContextMenu.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        /// <summary>
+        /// 社員リスト
+        /// </summary>
+        private List<Employee> _employeeList;
+
+        /// <summary>
+        /// 社員リストプロパティ
+        /// </summary>
+        public List<Employee> EmployeeList
+        {
+            get { return _employeeList; }
+            set { SetProperty(ref _employeeList, value); }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MainPageViewModel()
         {
+            // 社員リストを生成する。
+            _employeeList = new List<Employee>
+            {
+                new Employee { FullName = "社員 太郎" },
+                new Employee { FullName = "社員 二郎" },
+                new Employee { FullName = "社員 三郎" },
+                new Employee { FullName = "社員 四郎" },
+                new Employee { FullName = "社員 五郎" },
+            };
+
         }
     }
 }
